2077d85dfb027fdfd210afa396550080c047a4c2
Mohd Kashan <kashan0716@gmail.com>
2025-09-01 19:16:58 +0530

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -48,7 +48,7 @@ function Sidebar() {
 
     try {
       const response = await fetch(
-        `https://quickgpt-backend.onrender.com/${newThreadId}`
+        `https://quickgpt-backend.onrender.com`
       );
       const res = await response.json();
       console.log(res);
@@ -58,12 +58,12 @@ function Sidebar() {
     } catch (err) {
       console.log(err);
     }
-  };
+  };S
 
   const deleteThread = async (threadId) => {
     try {
       const response = await fetch(
-        `https://quickgpt-backend.onrender.com/${threadId}`,
+        `https://quickgpt-backend.onrender.com`,
         { method: "DELETE" }
       );
       const res = await response.json();
